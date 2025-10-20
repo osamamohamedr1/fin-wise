f5e9a1a03cd2c809a381e4e6574e99f4290d6360
osamamohamedr1 <osamarizk262@gmail.com>
2025-10-20 21:04:42 +0300

    update

--- lib/finance_tracker.dart
+++ lib/finance_tracker.dart
@@ -14,7 +14,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class FinanceTracker extends StatelessWidget {
   const FinanceTracker({super.key});
-
+  //
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
