961f67a771e5bae2d25611c8369b5e62a09a67b2
osamamohamedr1 <osamarizk262@gmail.com>
2025-10-23 20:47:52 +0300

    add sentry to test error tracking

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,3 +1,6 @@
+import 'dart:developer';
+import 'dart:ui';
+
 import 'package:easy_localization/easy_localization.dart';
 import 'package:finance_wise/core/models/transaction_model.dart';
 import 'package:finance_wise/core/utils/constants.dart';
@@ -6,10 +9,12 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hive_flutter/adapters.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  SentryWidgetsFlutterBinding.ensureInitialized();
   await ScreenUtil.ensureScreenSize();
   await EasyLocalization.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
@@ -18,13 +23,43 @@ void main() async {
   Hive.registerAdapter(TransactionModelAdapter());
   await Hive.openBox<TransactionModel>(transactionsBox);
   await Hive.openBox(themeBox);
+  await SentryFlutter.init(
+    (options) {
+      options.dsn =
+          'https://4295f0010351ce8085879cd1d0e6b273@o4510239235112960.ingest.us.sentry.io/4510239244943360';
 
-  runApp(
-    EasyLocalization(
-      path: 'assets/translations',
-      supportedLocales: [Locale('en'), Locale('ar')],
-      fallbackLocale: const Locale('en'),
-      child: const FinanceTracker(),
-    ),
+      options.sendDefaultPii = true;
+      // Set sample rate to 1.0 to capture 100% of transactions for performance monitoring
+      options.tracesSampleRate = 1.0;
+      // Enable debug mode to see what's happening (remove in production)
+      options.debug = true;
+      // Capture failed HTTP requests
+      options.captureFailedRequests = true;
+      log('Sentry Initialized');
+    },
+    appRunner: () {
+      FlutterError.onError = (FlutterErrorDetails details) async {
+        await Sentry.captureException(
+          details.exception,
+          stackTrace: details.stack,
+        );
+        log('Flutter Error: ${details.exception}');
+      };
+
+      PlatformDispatcher.instance.onError = (error, stack) {
+        Sentry.captureException(error, stackTrace: stack);
+        log('Async Error: $error');
+        return true;
+      };
+
+      runApp(
+        EasyLocalization(
+          path: 'assets/translations',
+          supportedLocales: [Locale('en'), Locale('ar')],
+          fallbackLocale: const Locale('en'),
+          child: const FinanceTracker(),
+        ),
+      );
+    },
   );
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,7 +9,9 @@ import firebase_analytics
 import firebase_core
 import firebase_crashlytics
 import flutter_secure_storage_macos
+import package_info_plus
 import path_provider_foundation
+import sentry_flutter
 import share_plus
 import shared_preferences_foundation
 
@@ -18,7 +20,9 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseCrashlyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCrashlyticsPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SentryFlutterPlugin.register(with: registry.registrar(forPlugin: "SentryFlutterPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
